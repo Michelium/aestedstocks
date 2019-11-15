93cdac4261d6239751dcbdec201fa8595f6e8795
Michelium <michelhamelink@gmail.com>
2019-11-16 00:15:23 +0100

    controllers added, front structure added

---
+++ src/Controller/TrainerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrainerController extends AbstractController
+{
+    /**
+     * @Route("/trainer", name="trainer")
+     */
+    public function index()
+    {
+        return $this->render('trainer/index.html.twig', [
+            'controller_name' => 'TrainerController',
+        ]);
+    }
+}
