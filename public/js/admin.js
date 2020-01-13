e900c8d90f4922c7b3f5df92ee3094d33dbd6943
Michelium <michelhamelink@gmail.com>
2020-01-13 19:56:30 +0100

    initial commit

---
+++ public/js/admin.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    // function titleToSlug() {
+    //     let titlefield = $('.titlefield');
+    //     let slugfield = $('.slugfield');
+    //
+    //     titlefield.on('change', function () {
+    //         let Text = $(this).val();
+    //         Text = Text.toLowerCase();
+    //         Text = Text.replace(/[^a-zA-Z0-9]+/g,'-');
+    //         slugfield.val(Text);
+    //     })
+    // }
+    //
+    //
+    // titleToSlug();
+
+});
+
